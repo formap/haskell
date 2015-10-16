a4c261c0a350e6d28e2516f84e387185a4d07e97
Maria Florencia Tarditti <maria.florencia.tarditti@c6s309pc64.fib.upc.es>
2015-10-16 13:57:43 +0200

    More ordenation functions

--- LP/Lab1/Ordenacio.hs
+++ LP/Lab1/Ordenacio.hs
@@ -7,3 +7,28 @@ insert (x:xs) y
 isort::[Int]->[Int]
 isort [] = []
 isort (x:xs) = insert (isort xs) x
+
+removeFirstOccurrence::[Int]->Int->Bool->[Int]
+removeFirstOccurrence [] y z = []
+removeFirstOccurrence (x:xs) y z = 
+  if x == y && z == False then
+    removeFirstOccurrence xs y True
+  else
+    [x] ++ removeFirstOccurrence xs y z
+
+remove::[Int]->Int->[Int]
+remove [] y = []
+remove (x:xs) y = removeFirstOccurrence (x:xs) y False
+
+ssort::[Int]->[Int]
+ssort [] = []
+ssort (x:xs) = [minimum (x:xs)] ++ ssort (remove (x:xs) (minimum (x:xs)))
+
+merge::[Int]->[Int]->[Int]
+merge (x:xs) [] = (x:xs)
+merge [] (y:ys) = (y:ys)
+merge (x:xs) (y:ys) = 
+  if x <= y then
+    [x] ++ [y] ++ merge xs ys
+  else
+    [y] ++ [x] ++ merge xs ys
