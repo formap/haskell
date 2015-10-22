77a7aab11d220975ce62e948f37c53114ce9304e
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2015-10-22 23:56:36 +0200

    Ordenacio remove improved and merge fixed

--- LP/Lab1/Ordenacio.hs
+++ LP/Lab1/Ordenacio.hs
@@ -10,30 +10,28 @@ isort (x:xs) = insert (isort xs) x
 
 remove::[Int]->Int->[Int]
 remove [] y = []
-remove (x:xs) y =
-  if x == y then
-    xs
-  else
-    x : (remove xs y)
+remove (x:xs) y
+  | x == y = xs
+  | otherwise = x : (remove xs y)
 
 ssort::[Int]->[Int]
 ssort [] = []
-ssort (x:xs) = [minimum (x:xs)] ++ ssort (remove (x:xs) (minimum (x:xs)))
+ssort (x:xs) = [min] ++ ssort (remove (x:xs) min)
+  where min = minimum (x:xs)
 
 merge::[Int]->[Int]->[Int]
 merge [] [] = []
 merge (x:xs) [] = (x:xs)
 merge [] (y:ys) = (y:ys)
-merge (x:xs) (y:ys) =
-  if x <= y then
-    [x] ++ merge xs (y:ys)
-  else
-    [y] ++ merge (x:xs) ys
+merge (x:xs) (y:ys)
+  | x <= y = [x] ++ merge xs (y:ys)
+  | otherwise = [y] ++ merge (x:xs) ys
 
 msort::[Int]->[Int]
 msort [] = []
+msort [x] = [x]
 msort (x:xs) =
-  let (y,z) = splitAt (div (length (x:xs) + 1) 2) (x:xs) in merge (ssort y) (ssort z)
+  let (y,z) = splitAt (div (length (x:xs) + 1) 2) (x:xs) in merge (msort y) (msort z)
 
 qsort::[Int]->[Int]
 qsort [] = []
