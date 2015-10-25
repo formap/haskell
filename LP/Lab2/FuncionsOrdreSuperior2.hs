c3519b01bdd1fb445549d8f8f6958725bc6dbb40
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2015-10-25 22:43:31 +0100

    Higher order functions exercise two finished

--- LP/Lab2/FuncionsOrdreSuperior2.hs
+++ LP/Lab2/FuncionsOrdreSuperior2.hs
@@ -5,8 +5,15 @@ flatten (x:xs) = foldr (++) [] (x:xs)
 myLength::String->Int
 myLength [] = 0
 myLength (x:xs) = 1 + myLength xs
---myLength (x:xs) = map (\x -> if x == [] then 0 else 1 + myLength xs)
 
 myReverse::[Int]->[Int]
 myReverse [] = []
-myReverse (x:xs) = myReverse xs ++ [x]
+myReverse (x:xs) = foldl (\y z -> z : y) [] (x:xs)
+
+countIn::[[Int]]->Int->[Int]
+countIn [] n = []
+countIn (x:xs) n = map (\y -> length (filter (==n) y)) (x:xs)
+
+firstWord::String->String
+firstWord [] = []
+firstWord (x:xs) = head (words (x:xs))
